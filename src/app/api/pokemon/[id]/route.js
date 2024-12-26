987a95a45d5413092312e6e98ef79c0e209892c7
Dallin Moak <dallinmoak@gmail.com>
2024-12-25 20:53:49 -0700

    add cors allow to all endpoint headers

--- src/app/api/pokemon/[id]/route.js
+++ src/app/api/pokemon/[id]/route.js
@@ -3,18 +3,36 @@ import { getOne, updateOne, deleteOne } from "@/models/pokemon";
 export async function GET(request, { params }) {
   const id = (await params).id;
   const response = await getOne(id);
-  return new Response(JSON.stringify(response));
+  return new Response(JSON.stringify(response), {
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
+      'Access-Control-Allow-Headers': 'Content-Type, Authorization',
+    },
+  });
 }
 
 export async function PUT(request, { params }) {
   const id = (await params).id;
   const pokemon = await request.json();
   const response = await updateOne(id, pokemon);
-  return new Response(JSON.stringify(response));
+  return new Response(JSON.stringify(response), {
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
+      'Access-Control-Allow-Headers': 'Content-Type, Authorization',
+    },
+  });
 }
 
 export async function DELETE(request, { params }) {
   const id = (await params).id;
   const response = await deleteOne(id);
-  return new Response(JSON.stringify(response));
+  return new Response(JSON.stringify(response), {
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
+      'Access-Control-Allow-Headers': 'Content-Type, Authorization',
+    },
+  });
 }
--- src/app/api/pokemon/route.js
+++ src/app/api/pokemon/route.js
@@ -2,11 +2,23 @@ import { add, getAll } from "@/models/pokemon";
 
 export async function GET(request) {
   const response = await getAll();
-  return new Response(JSON.stringify(response));
+  return new Response(JSON.stringify(response), {
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
+      'Access-Control-Allow-Headers': 'Content-Type, Authorization',
+    },
+  });
 }
 
 export async function POST(request) {
   const pokemon = await request.json();
   const name = await add(pokemon);
-  return new Response(JSON.stringify(name));
+  return new Response(JSON.stringify(name), {
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
+      'Access-Control-Allow-Headers': 'Content-Type, Authorization',
+    },
+  });
 }
